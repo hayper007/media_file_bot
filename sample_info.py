a74450c061770df389a6dfed0e42ce21a55f2fed
Hayper <68509800+kingboyss@users.noreply.github.com>
2021-05-29 00:06:56 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -30,4 +30,4 @@ Here you can search files in inline mode. Just press follwing buttons and start 
 """
 
 SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
-INVITE_MSG = 'Please join @.... to use this bot'
+INVITE_MSG = 'Please join @Movie_Storage1 to use this bot'
